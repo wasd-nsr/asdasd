44005a7ee5b2c52db898b67b592e28434eb5a35f
wasd-nsr <94560179+wasd-nsr@users.noreply.github.com>
2023-06-19 00:08:20 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -21,13 +21,8 @@ def configure_mongo():
     load_dotenv()
     MONGO_USER = os.getenv("MONGO_USER")
     MONGO_PASS = os.getenv("MONGO_PASS")
-    
-    
-    print("user",MONGO_USER)
-    print("pw",MONGO_PASS)
     connection_string = f"mongodb+srv://{MONGO_USER}:{MONGO_PASS}@cluster0.smd7qvr.mongodb.net/?retryWrites=true&w=majority&wtimeoutMS=10000"
-        
-    
+            
     cluster = MongoClient(connection_string)
     db = cluster["kinatu"]
     collection = db["files_info"]
